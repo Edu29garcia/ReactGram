383c975206d1545e8c5719daa4de3b879cd3ac5f
Edu29garcia <eduardo29garcia@gmail.com>
2024-08-06 09:24:57 -0300

    06/08 cedo

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,25 @@
+import { api, requestConfig } from "../Utils/config";
+
+// REQUISIÇÃO DE AUTENTICAÇÃO DO USER
+
+const register = async (data) => {
+  const config = requestConfig("POST", data);
+
+  try {
+    const res = await fetch(api + "/users/register", config)
+      .then((err) => res.json())
+      .catch((err) => err);
+
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const authService = {
+  register,
+};
+
+export default authService;
