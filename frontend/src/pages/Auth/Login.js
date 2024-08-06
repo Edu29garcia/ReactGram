24fa5656c8122dc05bf6ed53731e8344889d5b34
Edu29garcia <eduardo29garcia@gmail.com>
2024-08-06 15:57:00 -0300

    06/08 tarde

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -11,6 +11,9 @@ import { useSelector, useDispatch } from "react-redux";
 // Redux
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -20,8 +23,18 @@ const Login = () => {
       <h2>ReactGram</h2>
       <p className="subtitle">Faça login para ver o que há de novo</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
         <input type="submit" value="Entrar" />
       </form>
       <p>
